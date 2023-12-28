4f65a2c9bbcaa337d48182e6aad659b88806141a
sara <Sarih9030@gmail.com>
2023-12-28 09:58:28 +0200

    arrange code

--- src/Cat.java
+++ src/Cat.java
@@ -1,19 +1,23 @@
-public class Cat extends Animal implements Land{
+public class Cat extends Animal implements Land {
     private static int numberOfLegs = 4;
+
     public Cat(boolean mammals, boolean carnivorous, int mood) {
-        super(mammals, carnivorous, mood );
+        super(mammals, carnivorous, mood);
     }
-@Override
-public void sayHello(){
-    System.out.println("meow~");
-}
+
     @Override
-    public void sayHello(int mood){
-        if(MOOD.values()[mood] == MOOD.MOOD_HAPPY)
+    public void sayHello() {
+        System.out.println("meow~");
+    }
+
+    @Override
+    public void sayHello(int mood) {
+        if (MOOD.values()[mood] == MOOD.MOOD_HAPPY)
             System.out.println("purr, purr");
         else
             System.out.println("hiss");
     }
+
     @Override
     public int getNumberOfLegs() {
         return numberOfLegs;
